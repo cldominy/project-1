9adb127eb275c70d59d5dda5a78910ca9e19d480
Chris Dominy <cdominy@utexas.edu>
2020-09-08 19:34:01 -0500

    Updated the readme file and created a contact page

--- assets/materialize.js
+++ assets/materialize.js
@@ -7,3 +7,6 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.dropdown-trigger');
   var instances = M.Dropdown.init(elems, {});
 });
+
+$('#message').val('');
+M.textareaAutoResize($('#message'));
